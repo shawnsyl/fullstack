c6b3d6118a9de0db6810615694e760a96c6f3bf2
shawnsyl <shawn.lee9110@gmail.com>
2021-09-30 13:10:32 -0600

    setup ts

---
+++ lireddit-web/src/pages/register.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Formik } from "formik";
+
+interface registerProps {}
+
+const Register: React.FC<registerProps> = ({}) => {
+    return <div>register page</div>;
+};
+
+export default Register;
