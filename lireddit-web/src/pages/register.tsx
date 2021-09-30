735582d8c214d42824f9abae1224e2b5bfb3913b
shawnsyl <shawn.lee9110@gmail.com>
2021-09-30 16:06:55 -0600

    add register page

--- lireddit-web/src/pages/register.tsx
+++ lireddit-web/src/pages/register.tsx
@@ -1,10 +1,74 @@
 import React from "react";
-import { Formik } from "formik";
+import { Formik, Form } from "formik";
+import {
+  FormControl,
+  FormLabel,
+  FormHelperText,
+} from "@chakra-ui/form-control";
+import { Input } from "@chakra-ui/input";
+
+import { Wrapper } from "../components/Wrapper";
+import { InputField } from "../components/InputField";
+import { Box } from "@chakra-ui/layout";
+import { Button } from "@chakra-ui/button";
+import { useMutation } from "urql";
 
 interface registerProps {}
 
+const REGISTER_MUTATION = `
+mutation Register($username: String!, $password: String!) {
+  register(options: { username: $username, password: $password }) {
+    errors {
+      field
+      message
+    }
+    user {
+      id
+      username
+    }
+  }
+}
+`;
+
 const Register: React.FC<registerProps> = ({}) => {
-    return <div>register page</div>;
+  const [, register] = useMutation(REGISTER_MUTATION);
+  return (
+    <Wrapper variant="small">
+      <Formik
+        initialValues={{ username: "", password: "" }}
+        onSubmit={async (values) => {
+          const response = await register(values);
+        }}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+            <InputField
+              name="username"
+              placeholder="Username"
+              label="Username"
+            />
+            <Box mt={4}>
+              <InputField
+                name="password"
+                placeholder="Password"
+                label="Password"
+                type="password"
+              />
+            </Box>
+            <Button
+              mt={4}
+              type="submit"
+              isLoading={isSubmitting}
+              colorScheme="teal"
+            >
+              Register
+            </Button>
+            {/* <FormErrorMessage>{form.errors.name}</FormErrorMessage> */}
+          </Form>
+        )}
+      </Formik>
+    </Wrapper>
+  );
 };
 
 export default Register;
